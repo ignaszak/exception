92b1081803be1d013f1154e673811eecb470fac4
ignaszak <tomek.ignaszak@gmail.com>
2016-03-19 16:42:03 +0100

    Remove unnecessary 'break'

--- src/Handler/ErrorHandler.php
+++ src/Handler/ErrorHandler.php
@@ -2,11 +2,10 @@
 /**
  * phpDocumentor
  *
- * PHP Version 5.5
+ * PHP Version 7.0
  *
  * @copyright 2015 Tomasz Ignaszak
  * @license   http://www.opensource.org/licenses/mit-license.php MIT
- * @link      http://phpdoc.org
  */
 
 namespace Ignaszak\Exception\Handler;
@@ -15,7 +14,6 @@ namespace Ignaszak\Exception\Handler;
  * Provides methods to handle errors
  *
  * @author Tomasz Ignaszak <tomek.ignaszak@gmail.com>
- * @link https://github.com/ignaszak/exception/blob/master/src/Handler/ErrorHandler.php
  *
  */
 class ErrorHandler extends Handler
@@ -81,58 +79,40 @@ class ErrorHandler extends Handler
         switch ($errorNumber) {
             case 0:
                 return "";
-            break;
             case E_ERROR:
                 return "Fatal error";
-            break;
             case E_WARNING:
                 return "Warning";
-            break;
             case E_PARSE:
                 return "Parse error";
-            break;
             case E_NOTICE:
                 return "Notice";
-            break;
             case E_CORE_ERROR:
                 return "Core error";
-            break;
             case E_CORE_WARNING:
                 return "Core warning";
-            break;
             case E_COMPILE_ERROR:
                 return "Compile error";
-            break;
             case E_COMPILE_WARNING:
                 return "Compile warning";
-            break;
             case E_USER_ERROR:
                 return "User error";
-            break;
             case E_USER_WARNING:
                 return "User warning";
-            break;
             case E_USER_NOTICE:
                 return "User notice";
-            break;
             case E_STRICT:
                 return "Strict notice";
-            break;
             case E_RECOVERABLE_ERROR:
                 return "Recoverable error";
-            break;
             case E_DEPRECATED:
                 return "Deprecated error";
-            break;
             case E_USER_DEPRECATED:
                 return "User deprecated error";
-            break;
             case E_RECOVERABLE_ERROR:
                 return "Recoverable error";
-            break;
             default:
                 return "Unknown error ($errorNumber)";
-            break;
         }
     }
 }
